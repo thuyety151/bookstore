b251fe3ed35d71341a14d6a2c7a07325d72799e6
Thuyety <thuyety15@gmail.com>
2021-08-09 23:30:27 +0700

    init

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import ContainedButtons from './components/ContainedButtons'
+function App() {
+  return (
+    <div className="App">
+      <h3>hihi</h3>
+      <ContainedButtons />
+    </div>
+  );
+}
+
+export default App;
