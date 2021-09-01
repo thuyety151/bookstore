a604eeea961cb6ec97342a0b0a6862a9e986c86c
TruongNguyen1203 <nguyentruong.120300@gmail.com>
2021-09-01 22:08:30 +0700

    deals of week

---
+++ TLCN/client-app/src/components/SlideShow.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Slide } from 'react-slideshow-image';
+import 'react-slideshow-image/dist/styles.css';
+import image from '../images/slide_5.png';
+
+const slideImages = [
+  image,
+  image,
+  image
+];
+
+const Slideshow = () => {
+    return (
+      <div className="slide-container">
+        <Slide>
+          <div className="each-slide">
+            <div style={{'backgroundImage': `url(${slideImages[0]})`}}>
+              <span>Slide 1</span>
+            </div>
+          </div>
+          <div className="each-slide">
+            <div style={{'backgroundImage': `url(${slideImages[1]})`}}>
+              <span>Slide 2</span>
+            </div>
+          </div>
+          <div className="each-slide">
+            <div style={{'backgroundImage': `url(${slideImages[2]})`}}>
+              <span>Slide 3</span>
+            </div>
+          </div>
+        </Slide>
+      </div>
+    )
+};
+
+export default Slideshow;
