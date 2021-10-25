1583e6e5819a0b7b88a75077ffb64252ed2019ae
Y Nguyen <thuyety15@gmail.com>
2021-10-25 23:12:24 +0700

    [API-Address]: Set up API GHN

--- services/web/src/config/config.ts
+++ services/web/src/config/config.ts
@@ -1,19 +1,20 @@
-/** @format */
-
 function assertEnv(value: string | undefined, key: string): string {
-  // const variables = {
-  //   API_HOST: process.env.REACT_APP_API_HOST,
-  //   ...require("dotenv").config().parsed,
-  // };
   if (!value) {
     throw new Error(`Environment ${key} doesn't exist`);
   }
-
   return value;
 }
 
 const Config = {
   apiHost: assertEnv(process.env.REACT_APP_API_HOST, "REACT_APP_API_HOST"),
+  apiAddressHost: assertEnv(
+    process.env.REACT_APP_API_GHN_ADDRESS,
+    "REACT_APP_API_GHN_ADDRESS"
+  ),
+  apiAddressKey: assertEnv(
+    process.env.REACT_APP_API_GHN_KEY,
+    "REACT_APP_API_GHN_KEY"
+  ),
 };
 
 export default Config;
