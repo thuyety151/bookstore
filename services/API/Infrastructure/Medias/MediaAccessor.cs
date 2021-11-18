696776975711d1d0823d24e1d04efb15b91913dc
TruongNguyen1203 <nguyentruong.120300@gmail.com>
2021-11-18 15:24:30 +0700

    [CRUD book api] Media accessor

--- services/API/Infrastructure/Medias/MediaAccessor.cs
+++ services/API/Infrastructure/Medias/MediaAccessor.cs
@@ -1,7 +1,60 @@
+using System;
+using System.Threading.Tasks;
+using Application.Interface;
+using Application.Medias;
+using CloudinaryDotNet;
+using CloudinaryDotNet.Actions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Options;
+
 namespace Infrastructure.Medias
 {
-    public class MediaAccessor
+    public class MediaAccessor : IMediaAccessor
     {
-        
+        private readonly Cloudinary _cloudinary;
+
+        public MediaAccessor(IOptions<CloudinarySetting> config)
+        {
+            var account = new Account(config.Value.CloudName, config.Value.ApiKey, config.Value.ApiSecret);
+            _cloudinary = new Cloudinary(account);
+        }
+
+        public async Task<MediaUploadResult> AddMedia(FormFile file)
+        {
+            if (file.Length > 0)
+            {
+                await using var stream = file.OpenReadStream();
+
+                var uploadParams = new ImageUploadParams()
+                {
+                    File = new FileDescription(file.FileName, stream),
+                    Transformation = new Transformation().Height(1024).Width(1024).Crop("fill")
+                };
+
+                var uploadResult = await _cloudinary.UploadAsync(uploadParams);
+
+                if (uploadResult.Error != null)
+                {
+                    throw new Exception(uploadResult.Error.Message);
+                }
+                else
+                {
+                    return new MediaUploadResult()
+                    {
+                        PublicId = uploadResult.PublicId,
+                        Url = uploadResult.SecureUrl.ToString()
+                    };
+                }
+            }
+            return null;
+        }
+
+        public async Task<string> DeleteMedia(string publicId)
+        {
+            var deleteParams = new DeletionParams(publicId);
+            var result = await _cloudinary.DestroyAsync(deleteParams);
+
+            return result.Result == "ok" ? result.Result : null;
+        }
     }
 }
