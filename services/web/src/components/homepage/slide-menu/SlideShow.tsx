d41a3f3fb47c4b3307fe51092f25c69a2dd05810
Y Nguyen <thuyety15@gmail.com>
2021-12-02 23:25:30 +0700

    [bug-client]: fix css slideshow:

--- services/web/src/components/homepage/slide-menu/SlideShow.tsx
+++ services/web/src/components/homepage/slide-menu/SlideShow.tsx
@@ -1,3 +1,4 @@
+import { Grid } from "@material-ui/core";
 import React from "react";
 import AliceCarousel from "react-alice-carousel";
 import "react-alice-carousel/lib/alice-carousel.css";
@@ -5,14 +6,12 @@ import image1 from "../../../assets/images/menu-1.png";
 import image2 from "../../../assets/images/menu-2.png";
 import image3 from "../../../assets/images/menu-3.png";
 
-
 const handleDragStart = (e: any) => e.preventDefault();
 
-
 const items = [
-  <img src={image1} alt="menu1" onDragStart={handleDragStart} max-width="70%" width="1450px"/>,
-  <img src={image2} alt="menu2" onDragStart={handleDragStart} width="1450px" />,
-  <img src={image3} alt="menu3" onDragStart={handleDragStart} width="1450px"/>,
+  <img src={image1} alt="menu1" onDragStart={handleDragStart} width="100%" />,
+  <img src={image2} alt="menu2" onDragStart={handleDragStart} width="100%" />,
+  <img src={image3} alt="menu3" onDragStart={handleDragStart} width="100%" />,
 ];
 
 const responsive = {
@@ -23,10 +22,17 @@ const responsive = {
 
 const SlideEffect: React.FC = () => {
   return (
-    <AliceCarousel mouseTracking disableDotsControls items={items} responsive={responsive}/>
+    <Grid container justifyContent="center">
+      <Grid item xs={9}>
+        <AliceCarousel
+          mouseTracking
+          disableDotsControls
+          items={items}
+          responsive={responsive}
+        />
+      </Grid>
+    </Grid>
   );
-
 };
 
 export default SlideEffect;
-
