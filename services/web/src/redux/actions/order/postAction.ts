9e2c69639ae739b23a6568a6437e65a03666a70f
Y Nguyen <thuyety15@gmail.com>
2021-12-04 07:58:33 +0700

    [bug-client]: fix price when create order

--- services/web/src/redux/actions/order/postAction.ts
+++ services/web/src/redux/actions/order/postAction.ts
@@ -1,7 +1,9 @@
 import apiGHN from "../../../boot/apiGHN";
 import api from "../../../boot/axios";
 import { formatAddress } from "../../../helper/format";
+import { shopAddress } from "../../../mocks/shopInfo";
 import { NAME_ACTIONS } from "../../constants/order/actionTypes";
+import { total } from "../../reducers/orderReducer";
 import store from "../../store";
 
 export type CreateOrderProps = {
@@ -22,7 +24,7 @@ export const createOrder =
     };
     const response = await api.post("/orders/create", data);
 
-    if (response.data.value) {
+    if (response.data.isSuccess) {
       dispatch({ type: NAME_ACTIONS.CREATE_ORDER.CREATE_ORDER_SUCCESS });
 
       const order = {
@@ -30,26 +32,32 @@ export const createOrder =
         note: props.note,
         return_phone: address.phone,
         return_address: formatAddress(address),
-        return_district_id: 1566,
-        return_ward_code: "510104",
+        return_district_id: shopAddress.district_id,
+        return_ward_code: shopAddress.ward_code,
         to_name: address.firstName + " " + address.lastName,
         to_phone: address.phone,
         to_address: formatAddress(address),
-        to_district_id: 1566,
+        to_district_id: address.districtId,
         to_ward_code: address.wardCode,
         required_note: "KHONGCHOXEMHANG",
         deliver_station_id: null,
         weight: 200,
-        order_value: 200000,
+        order_value:
+          state.cart.itemToCheckOut.length === 0
+            ? Math.round(total(state.cart.data) * 23000)
+            : Math.round(total(state.cart.itemToCheckOut) * 23000),
         service_type_id: state.order.currentService.service_type_id,
         service_id: state.order.currentService.service_id,
-        insurance_value: 100000,
+        insurance_value:
+          state.cart.itemToCheckOut.length === 0
+            ? Math.round(total(state.cart.data) * 23000)
+            : Math.round(total(state.cart.itemToCheckOut) * 23000),
         cod_amount: 200000,
         pick_station_id: 1444,
         items: state.cart.itemToCheckOut.map((item) => {
           return {
             name: item.productName,
-            code: "hi",
+            // code: "hi",
             quantity: item.quantity,
             price: item.price,
             category: {
@@ -58,7 +66,6 @@ export const createOrder =
           };
         }),
       };
-
       try {
         const createDelivery = await apiGHN.post(
           "/v2/shipping-order/create",
@@ -101,17 +108,17 @@ export const createOrder =
         /**
          * Delete order when create GHN fail
          */
-        // await api.delete("/orders", {
-        //   params: {
-        //     id: response.data.value,
-        //   },
-        // });
-        // props.onFailure("Create order fail!");
-        // dispatch({
-        //   type: NAME_ACTIONS.CREATE_DELIVERY_FOR_ORDER
-        //     .CREATE_DELIVERY_FOR_ORDER_FAIL,
-        //   message: error.message,
-        // });
+        await api.delete("/orders", {
+          params: {
+            id: response.data.value,
+          },
+        });
+        props.onFailure(error.response.data.message);
+        dispatch({
+          type: NAME_ACTIONS.CREATE_DELIVERY_FOR_ORDER
+            .CREATE_DELIVERY_FOR_ORDER_FAIL,
+          message: error.message,
+        });
       }
     } else {
       props.onFailure(response.data.error);
