6e8f1fa244f886a6b70a4ef06262733dda662ee9
TruongNguyen1203 <nguyentruong.120300@gmail.com>
2021-12-10 00:11:57 +0700

    [Facebook login] API

--- services/API/API/Controllers/AccountController.cs
+++ services/API/API/Controllers/AccountController.cs
@@ -1,6 +1,4 @@
-using System;
 using System.Linq;
-using System.Net.Http;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using API.DTOs;
@@ -11,8 +9,6 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
-using Newtonsoft.Json;
 
 namespace API.Controllers
 {
@@ -23,19 +19,12 @@ namespace API.Controllers
         private readonly UserManager<AppUser> _userManager;
         private readonly SignInManager<AppUser> _signInManager;
         private readonly TokenService _tokenService;
-        private readonly IConfiguration _configuration;
-        private readonly HttpClient _httpClient;
         public AccountController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager,
-            TokenService tokenService, IConfiguration configuration)
+            TokenService tokenService)
         {
             _userManager = userManager;
             _signInManager = signInManager;
             _tokenService = tokenService;
-            _configuration = configuration;
-            _httpClient = new HttpClient()
-            {
-                BaseAddress = new Uri("https://graph.facebook.com")
-            };
         }
 
         [HttpPost("login")]
@@ -126,44 +115,6 @@ namespace API.Controllers
 
             return CreateUserObject(user);
         }
-
-        [HttpPost("facebook-login")]
-        public async Task<ActionResult<UserDto>> FacebookLogin(string accessToken)
-        {
-            var fbVerifyKeys = _configuration["Facebook:AppId"] + "|" + _configuration["Facebook:AppSecret"];
-
-            var verifyToken =
-                await _httpClient.GetAsync($"debug_token?input_token={accessToken}&access_token={fbVerifyKeys}");
-
-            if (!verifyToken.IsSuccessStatusCode) return Unauthorized();
-
-            var fbUrl = $"me?access_token={accessToken}&fields=name,email,picture.width(100).height(100)";
-
-            var response = await _httpClient.GetAsync(fbUrl);
-
-            if (!response.IsSuccessStatusCode) return Unauthorized();
-
-            var fbInfo = JsonConvert.DeserializeObject<dynamic>(await response.Content.ReadAsStringAsync());
-
-            var username = (string) fbInfo.id;
-
-            var user = await _userManager.Users.Include(x => x.Photo)
-                .FirstOrDefaultAsync(x => x.UserName == username);
-
-            if (user != null) return CreateUserObject(user);
-
-            user = new AppUser()
-            {
-                Email = (string) fbInfo.email,
-                UserName = (string) fbInfo.id,
-            };
-
-            var result = await _userManager.CreateAsync(user);
-
-            if (!result.Succeeded) return BadRequest("Problem creating user account");
-
-            return CreateUserObject(user);
-        }        
         [Authorize]
         [HttpGet]
         public async Task<ActionResult<UserDto>> GetCurrentUser()
--- services/web/src/service/auth.service.ts
+++ services/web/src/service/auth.service.ts
@@ -1,41 +1,73 @@
 import api from "../boot/axios";
 
 export const userService = {
-    login,
-    logout,
-    register,
-    updateAccount
+  login,
+  logout,
+  register,
+  updateAccount,
 };
 
-function login(email : any, password: any){
-    return api.post('/account/login', {email, password})
-                .then((response) => {
-                    if(response.data.token){
-                        localStorage.setItem("user", JSON.stringify(response.data));
-                    }
+function login(email: any, password: any) {
+  return api.post("/account/login", { email, password }).then((response) => {
+    if (response.data.token) {
+      localStorage.setItem("user", JSON.stringify(response.data));
+    }
 
-                    return response.data;
-                });
+    return response.data;
+  });
 }
-function register(firstName: any,lastName: any, email: any, password: any) {
-    return api.post('/account/register', {firstName,lastName, email, password})
-                .then((response) => {
-                    if(response.data.token){
-                        localStorage.setItem("user", JSON.stringify(response.data));
-                    }
-                    return response.data;
-                })
+function register(firstName: any, lastName: any, email: any, password: any) {
+  return api
+    .post("/account/register", { firstName, lastName, email, password })
+    .then((response) => {
+      if (response.data.token) {
+        localStorage.setItem("user", JSON.stringify(response.data));
+      }
+      return response.data;
+    });
 }
-function logout(){
-    localStorage.removeItem("user");
+function logout() {
+  localStorage.removeItem("user");
 }
 
-function updateAccount(firstName: any,lastName: any, currentPassword: any, newPassword: any) {
-    return api.post('/account/update-account', {firstName,lastName,currentPassword, newPassword})
-                .then((response) => {
-                    if(response.data.token){
-                        localStorage.setItem("user", JSON.stringify(response.data));
-                    }
-                    return response.data;
-                })
+function updateAccount(
+  firstName: any,
+  lastName: any,
+  currentPassword: any,
+  newPassword: any
+) {
+  return api
+    .post("/account/update-account", {
+      firstName,
+      lastName,
+      currentPassword,
+      newPassword,
+    })
+    .then((response) => {
+      if (response.data.token) {
+        localStorage.setItem("user", JSON.stringify(response.data));
+      }
+      return response.data;
+    });
 }
+
+export type FacebookLoginType = {
+  accessToken: string;
+  onSuccess: () => void;
+  onFailure: (error: any) => void;
+};
+export const loginFacebook = (props: FacebookLoginType) => async (
+  dispatch: any
+) => {
+  var response = await api.post("/facebook-login", props.accessToken);
+
+  console.log(JSON.stringify(response));
+
+  if (response.data.token) {
+    localStorage.setItem("user", JSON.stringify(response.data));
+    props.onSuccess();
+  }
+  else {
+      props.onFailure("Unauthorize")
+  }
+};
