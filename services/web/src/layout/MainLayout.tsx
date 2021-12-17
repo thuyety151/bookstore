d12699d061a3ebd85f5c4eb376714f99362055ce
Y Nguyen <thuyety15@gmail.com>
2021-12-17 12:09:57 +0700

    [client]: responsive footer

--- services/web/src/layout/MainLayout.tsx
+++ services/web/src/layout/MainLayout.tsx
@@ -7,7 +7,7 @@ import HeaderComponent from "../components/layout/navbar/HeaderComponent";
 import NavBarComponent from "../components/layout/navbar/NavBar";
 import SideBarComponent from "../components/layout/sidebar/MainSideBar";
 import PrivateRoute from "../components/route/PrivateRoute";
-// import FooterComponent from "../components/layout/footer/MainFooter";
+import FooterComponent from "../components/layout/footer/MainFooter";
 import { AppBar } from "@material-ui/core";
 
 const MainLayout: React.FC = () => {
@@ -29,20 +29,20 @@ const MainLayout: React.FC = () => {
       />
       <MainShoppingCart openCart={openCart} setOpenCart={setOpenCart} />
       {/* <ScrollToTop> */}
-        <Switch>
-          {routeMainLayout.map((route, index) => {
-            return (
-              <PrivateRoute
-                key={index}
-                exact={route.exact}
-                path={route.path}
-                component={route.component}
-              />
-            );
-          })}
-        </Switch>
+      <Switch>
+        {routeMainLayout.map((route, index) => {
+          return (
+            <PrivateRoute
+              key={index}
+              exact={route.exact}
+              path={route.path}
+              component={route.component}
+            />
+          );
+        })}
+      </Switch>
       {/* </ScrollToTop> */}
-      {/* <FooterComponent /> */}
+      <FooterComponent />
     </div>
   );
 };
