14dccac20d89c02ce4dcb797965f06683667a800
TruongNguyen1203 <nguyentruong.120300@gmail.com>
2022-04-15 01:18:44 +0700

    [BE] Review permission

--- services/API/Application/Review/Create.cs
+++ services/API/Application/Review/Create.cs
@@ -1,10 +1,12 @@
 using System;
+using System.Linq;
 using System.Security.Claims;
 using System.Threading;
 using System.Threading.Tasks;
 using Application.Core;
 using MediatR;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Persistence;
 
 
@@ -30,6 +32,14 @@ namespace Application.Review
             public async Task<Result<Unit>> Handle(Command request, CancellationToken cancellationToken)
             {
                 var userId = _httpContextAccessor.HttpContext.User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+                bool isHasPermissionToReview = _context.Orders.Include(x => x.Items).Any(x =>
+                    x.Items.Any(i => i.ProductId == request.Review.BookId && x.UserId.ToString() == userId));
+
+                if (!isHasPermissionToReview)
+                {
+                    return Result<Unit>.Failure("Unauthorized");
+                }
 
                 var review = request.Review;
                 review.UserId = userId;
