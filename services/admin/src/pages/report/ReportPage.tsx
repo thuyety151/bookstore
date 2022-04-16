b9ea217a8a916d79b5514a5051ff2c9dca019faa
Y Nguyen <thuyety15@gmail.com>
2022-04-17 00:44:30 +0700

    [report-options]: revamp ui for report options

--- services/admin/src/pages/report/ReportPage.tsx
+++ services/admin/src/pages/report/ReportPage.tsx
@@ -1,32 +1,17 @@
 import {
-  Button,
   createStyles,
   Grid,
-  LinearProgress,
-  List,
-  ListItem,
-  ListItemText,
   makeStyles,
   Paper,
-  TextField,
   Theme,
   Typography,
 } from "@material-ui/core";
 import { sum } from "lodash";
-import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { getReports } from "redux/actions/report/getActions";
+import React from "react";
+import { useSelector } from "react-redux";
 import { RootStore } from "redux/store";
 import Chart from "./components/Chart";
-import GetAppIcon from "@material-ui/icons/GetApp";
-import { CSVLink } from "react-csv";
 import "./styles.scss";
-import Box from "@mui/material/Box";
-import LocalizationProvider from "@mui/lab/LocalizationProvider";
-import DateRangePicker, { DateRange } from "@mui/lab/DateRangePicker";
-import AdapterDateFns from "@mui/lab/AdapterDateFns";
-import ContainedButton from "components/button/ContainedButton";
-import { format } from "date-fns";
 import HeaderPage from "components/headerPage/HeaderPage";
 import ReportOptions from "./components/ReportOptions";
 
@@ -50,47 +35,8 @@ export const reportOptions = [
 ];
 const ReportPage: React.FC = () => {
   const classes = useStyles();
-  const dispatch = useDispatch();
-  const [selectedIndex, setSelectedIndex] = React.useState(0);
-  const { data, } = useSelector((state: RootStore) => state.reports);
+  const { data } = useSelector((state: RootStore) => state.reports);
 
-  // const handleListItemClick = (
-  //   event: React.MouseEvent<HTMLDivElement, MouseEvent>,
-  //   index: number
-  // ) => {
-  //   setSelectedIndex(index);
-  // };
-  useEffect(() => {
-    dispatch(getReports(reportOptions[selectedIndex].value));
-  }, [dispatch, selectedIndex]);
-
-  const headers = [
-    { label: "Date", key: "name" },
-    { label: "Number of items sold", key: "itemsPurchased" },
-    { label: "Number of orders", key: "orderPlaced" },
-    { label: "Net sales amount", key: "netSale" },
-    { label: "Shipping amount", key: "shippingFee" },
-    { label: "Refund amount", key: "refunded" },
-  ];
-
-  const csvReport = {
-    fileName: "Report" + reportOptions[selectedIndex].value + ".csv",
-    headers: headers,
-    data: data,
-  };
-  const [date, setDate] = React.useState<DateRange<Date>>([null, null]);
-  const handleReportCustom = () => {
-    if (date.filter((x) => !!x).length !== 2) {
-      return;
-    }
-    dispatch(
-      getReports(
-        "custom",
-        date[0]?.toJSON() || new Date(),
-        date[1]?.toJSON() || new Date()
-      )
-    );
-  };
   return (
     <div className="report">
       <HeaderPage title="Dashboard" />
@@ -168,14 +114,6 @@ const ReportPage: React.FC = () => {
           {requesting && <LinearProgress />}
         </Paper>
       </Grid> */}
-      <Button
-        variant="contained"
-        // color="secondary"
-        className={classes.btnExport}
-      >
-        <GetAppIcon />
-        <CSVLink {...csvReport}> Export CSV</CSVLink>
-      </Button>
 
       <Grid container spacing={2}>
         <Grid item xs={2} className="quick-view">
--- services/admin/src/pages/report/components/ReportOptions.tsx
+++ services/admin/src/pages/report/components/ReportOptions.tsx
@@ -2,86 +2,136 @@ import {
   Box,
   Button,
   Grid,
+  LinearProgress,
   List,
   ListItem,
   ListItemText,
   TextField,
 } from "@material-ui/core";
 import LocalizationProvider from "@mui/lab/LocalizationProvider";
-import React from "react";
+import React, { useEffect } from "react";
 import { reportOptions } from "../ReportPage";
 import "../styles.scss";
 import AdapterDateFns from "@mui/lab/AdapterDateFns";
 import { format } from "date-fns";
 import { DateRange, DateRangePicker } from "@mui/lab";
+import { useDispatch, useSelector } from "react-redux";
+import { getReports } from "redux/actions/report/getActions";
+import ContainedButton from "components/button/ContainedButton";
+import GetAppIcon from "@material-ui/icons/GetApp";
+import { CSVLink } from "react-csv";
+import { RootStore } from "redux/store";
 
 const ReportOptions: React.FC = () => {
+  const dispatch = useDispatch();
+  const [selectedIndex, setSelectedIndex] = React.useState(0);
   const [date, setDate] = React.useState<DateRange<Date>>([null, null]);
+  const { data, requesting } = useSelector((state: RootStore) => state.reports);
+
+  const handleReportCustom = () => {
+    if (date.filter((x) => !!x).length !== 2) {
+      return;
+    }
+    dispatch(
+      getReports(
+        "custom",
+        date[0]?.toJSON() || new Date(),
+        date[1]?.toJSON() || new Date()
+      )
+    );
+  };
+
+  const handleListItemClick = (
+    event: React.MouseEvent<HTMLDivElement, MouseEvent>,
+    index: number
+  ) => {
+    setSelectedIndex(index);
+  };
+
+  useEffect(() => {
+    dispatch(getReports(reportOptions[selectedIndex].value));
+  }, [dispatch, selectedIndex]);
+
+  const headers = [
+    { label: "Date", key: "name" },
+    { label: "Number of items sold", key: "itemsPurchased" },
+    { label: "Number of orders", key: "orderPlaced" },
+    { label: "Net sales amount", key: "netSale" },
+    { label: "Shipping amount", key: "shippingFee" },
+    { label: "Refund amount", key: "refunded" },
+  ];
+
+  const csvReport = {
+    filename: "Report" + reportOptions[selectedIndex].value + ".csv",
+    headers: headers,
+    data: data,
+  };
 
   return (
-    <div className="report-options">
-      <List
-        component="nav"
-        aria-label="secondary mailbox folder"
-        // className={classes.flexContainer}
-      >
-        {reportOptions.map((item, index) => (
-          <ListItem
-            button
-            key={`select-${index}`}
-            selected={index === 1}
-            // onClick={(event) => handleListItemClick(event, index)}
-          >
-            <ListItemText primary={item.name} />
-          </ListItem>
-        ))}
-      </List>
-      <Grid style={{ display: "flex", marginTop: 6 }}>
-        <LocalizationProvider dateAdapter={AdapterDateFns}>
-          <DateRangePicker
-            startText="Date From"
-            endText="Date To"
-            value={date}
-            //   onChange={(newValue: any) => {
-            //     setDate(newValue);
-            //   }}
-            onChange={() => console.log("change")}
-            maxDate={new Date()}
-            renderInput={(startProps: any, endProps: any) => (
-              <React.Fragment>
-                <TextField
-                  {...startProps}
-                  variant="outlined"
-                  size="small"
-                  inputProps={{
-                    ...startProps.inputProps,
-                    value: date[0] ? format(date[0], "dd/MM/yyyy") : "",
-                    placeholder: "dd/MM/yyyy",
-                  }}
-                />
-                <Box sx={{ mx: 1 }}> to </Box>
-                <TextField
-                  {...endProps}
-                  inputProps={{
-                    ...startProps.inputProps,
-                    value: date[1] ? format(date[1], "dd/MM/yyyy") : "",
-                    placeholder: "dd/MM/yyyy",
-                  }}
-                  variant="outlined"
-                  size="small"
-                />
-              </React.Fragment>
-            )}
-          />
-        </LocalizationProvider>
-        <Button>Go</Button>
-        {/* <ContainedButton
+    <div className="report-options-container">
+      <div className="report-options">
+        <List component="nav" aria-label="secondary mailbox folder">
+          {reportOptions.map((item, index) => (
+            <ListItem
+              button
+              key={`select-${index}`}
+              selected={index === selectedIndex}
+              onClick={(event) => handleListItemClick(event, index)}
+              disabled={requesting}
+            >
+              <ListItemText primary={item.name} />
+            </ListItem>
+          ))}
+        </List>
+        <Grid style={{ display: "flex", marginTop: 6 }}>
+          <LocalizationProvider dateAdapter={AdapterDateFns}>
+            <DateRangePicker
+              startText="Date From"
+              endText="Date To"
+              value={date}
+              onChange={(newValue: any) => {
+                setDate(newValue);
+              }}
+              maxDate={new Date()}
+              renderInput={(startProps: any, endProps: any) => (
+                <React.Fragment>
+                  <TextField
+                    {...startProps}
+                    variant="outlined"
+                    size="small"
+                    inputProps={{
+                      ...startProps.inputProps,
+                      value: date[0] ? format(date[0], "dd/MM/yyyy") : "",
+                      placeholder: "dd/MM/yyyy",
+                    }}
+                  />
+                  <Box sx={{ mx: 1 }}> to </Box>
+                  <TextField
+                    {...endProps}
+                    inputProps={{
+                      ...startProps.inputProps,
+                      value: date[1] ? format(date[1], "dd/MM/yyyy") : "",
+                      placeholder: "dd/MM/yyyy",
+                    }}
+                    variant="outlined"
+                    size="small"
+                  />
+                </React.Fragment>
+              )}
+            />
+          </LocalizationProvider>
+          <ContainedButton
             text="Go"
-            // className={classes.customOpt}
             style={{ width: "fit-content" }}
-            // onClick={handleReportCustom}
-          /> */}
-      </Grid>
+            onClick={handleReportCustom}
+          />
+        </Grid>
+      </div>
+      {requesting && <LinearProgress className="mx-md" />}
+      <Button className="ml-none my-md">
+        <GetAppIcon />
+        <CSVLink {...csvReport}> Export CSV</CSVLink>
+      </Button>
     </div>
   );
 };
