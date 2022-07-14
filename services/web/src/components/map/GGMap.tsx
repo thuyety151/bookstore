007fa229458f5dae03796eada50e5a6cc0b71611
Y Nguyen <thuyety15@gmail.com>
2022-07-14 08:13:40 +0700

    [gg-map]: update package

--- services/web/src/components/map/GGMap.tsx
+++ services/web/src/components/map/GGMap.tsx
@@ -1,6 +1,8 @@
-import { Map, GoogleApiWrapper, Marker } from "google-maps-react";
+import { GoogleApiWrapper } from "google-maps-react";
 import { Grid } from "@material-ui/core";
 import Config from "../../config/config";
+import GoogleMapReact from "google-map-react";
+import "./styles.scss";
 
 export type GoogleMapType = {
   lng: number;
@@ -8,18 +10,26 @@ export type GoogleMapType = {
   google: any;
 };
 
+const Marker = (props: any) => (
+  <div className="marker">
+    <span>We're here</span>
+  </div>
+);
+
 const GGMap: React.FC<GoogleMapType> = (props) => (
   <Grid container justifyContent="center">
-    <Map
-      google={window.google}
-      zoom={20}
-      style={{
-        width: "100%",
-      }}
-      initialCenter={{ lat: props.lat, lng: props.lng }}
-    >
-      <Marker position={{ lat: props.lat, lng: props.lng }} />
-    </Map>
+    <div style={{ height: "100vh", width: "100%" }}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: "" }}
+        defaultZoom={20}
+        defaultCenter={{
+          lat: props.lat,
+          lng: props.lng,
+        }}
+      >
+        <Marker lng={props.lng} lat={props.lat} />
+      </GoogleMapReact>
+    </div>
   </Grid>
 );
 
